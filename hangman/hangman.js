3981b91cd04b69c14c5532757d6039ad454601a5
Iryna <iryna.knv@gmail.com>
2021-11-07 15:36:45 +0100

    fix: remaining letters computation
    Added correctGuesses variable to function updateGameStatus and made game work

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,6 +1,19 @@
-function pickWord(words) {
+function pickWord() {
+    var words = [
+        "lake",
+        "amazing",
+        "pancake",
+        "snow",
+        "javascript",
+        "art",
+        "landscape"
+    ];
     return words[Math.floor(Math.random() * words.length)];
 }
+
+var word = pickWord();
+var answerArray = setupAnswerArray(word);
+var remainingLetters = word.length;
 
 function setupAnswerArray(word) {
     var newArray = [];
@@ -19,14 +32,16 @@ function getGuess() {
 }
 
 function updateGameState(guess, word, answerArray) {
+    var correctGuesses = 0;
     for (var j = 0; j < word.length; j++) {
         if (word[j] === guess && answerArray[j] === "_") {
             answerArray[j] = guess;
+            correctGuesses++;
             console.log(answerArray[j]);
-            remainingLetters--;
-
-        } console.log("Remaining letters: " + remainingLetters);
-    } return answerArray;
+        } 
+        console.log("Remaining letters: " + remainingLetters);
+    } 
+    return correctGuesses;
 }
 
 function showAnswerAndCongratulatePlayer(answerArray) {
@@ -51,20 +66,5 @@ function playGame() {
         }
     }
 }
-
-var words = [
-    "lake",
-    "amazing",
-    "pancake",
-    "snow",
-    "javascript",
-    "art",
-    "landscape"
-];
-
-var word = pickWord(words);
-var answerArray = setupAnswerArray(word);
-var remainingLetters = word.length;
-
 playGame();
 showAnswerAndCongratulatePlayer(answerArray);
