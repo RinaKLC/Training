ec79d3d88ae6732075e9aa2b55b59a51ad671c27
Iryna <iryna.knv@gmail.com>
2021-11-07 15:01:21 +0100

    refactor: made game executable in browser
    Added index.html and restructured function layout

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,3 +1,57 @@
+function pickWord(words) {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+function setupAnswerArray(word) {
+    var newArray = [];
+    for (var i = 0; i < word.length; i++) {
+        newArray[i] = "_";
+    }
+    return newArray;
+}
+
+function showPlayerProgress(array) {
+    alert(array.join(" "));
+}
+
+function getGuess() {
+    return prompt("Guess a letter or click Cancel to stop game").toLowerCase();
+}
+
+function updateGameState(guess, word, answerArray) {
+    for (var j = 0; j < word.length; j++) {
+        if (word[j] === guess && answerArray[j] === "_") {
+            answerArray[j] = guess;
+            console.log(answerArray[j]);
+            remainingLetters--;
+
+        } console.log("Remaining letters: " + remainingLetters);
+    } return answerArray;
+}
+
+function showAnswerAndCongratulatePlayer(answerArray) {
+    alert(answerArray.join(" "));
+    alert("Good job! The answer was " + word);
+}
+
+
+function playGame() {
+    while (remainingLetters > 0) {
+        showPlayerProgress(answerArray);
+
+        var guess = getGuess();
+        if (guess === null) {
+            break;
+        } else if (guess.length !== 1) {
+            alert("Please enter only one letter");
+        } else {
+            var correctGuesses = updateGameState(guess, word, answerArray);
+            remainingLetters -= correctGuesses;
+            console.log(remainingLetters);
+        }
+    }
+}
+
 var words = [
     "lake",
     "amazing",
@@ -8,68 +62,9 @@ var words = [
     "landscape"
 ];
 
-function pickWord() {
-    return words[Math.floor(Math.random() * words.length)];
-}
-
-var word = pickWord();
-
-function setupAnswerArray(word) {
-    var newArray = [];
-    for (var i = 0; i < word.length; i++) {
-        newArray[i] = "_";
-    }
-    return newArray;
-}
-
+var word = pickWord(words);
 var answerArray = setupAnswerArray(word);
-
-function showPlayerProgress(array) {
-    alert(array.join(" "));
-}
-
 var remainingLetters = word.length;
 
-
-
-function getGuess() {
-    return prompt("Guess a letter or click Cancel to stop game").toLowerCase();
-}
-
-
-function updateGameState(guess, word, answerArray) {
-    for (var j = 0; j < word.length; j++) {
-        if (word[j] === guess && answerArray[j] === "_") {
-            answerArray[j] = guess;
-            console.log(answerArray[j]);
-            remainingletters--;
-
-        } console.log("Remaining letters: " + remainingLetters);
-    } return answerArray;
-
-}
-
-function showAnswerAndCongratulatePlayer(answerArray) {
-    alert(answerArray.join(" "));
-    alert("Good job! The answer was " + word);
-}
-
-
-while (remainingLetters > 0) {
-    showPlayerProgress(answerArray);
-
-    var guess = getGuess();
-    if (guess === null) {
-        break;
-    } else if (guess.length !== 1) {
-        alert("Please enter only one letter");
-    } else {
-        var correctGuesses = updateGameState(guess, word, answerArray);
-        remainingLetters -= correctGuesses;
-        console.log(remainingLetters);
-
-    }
-
-}
-
+playGame();
 showAnswerAndCongratulatePlayer(answerArray);
